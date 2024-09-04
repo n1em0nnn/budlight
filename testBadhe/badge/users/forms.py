0a7645be5f618ba5ec25f9dc7c3ebc20d8c18717
n1em0nnn <n1em0n@mail.ru>
2024-09-04 12:52:07 +0500

    users creating rework

--- testBadhe/badge/users/forms.py
+++ testBadhe/badge/users/forms.py
@@ -1,12 +1,13 @@
+from dataclasses import fields
+from django.contrib.auth.models import Group
 
 from django.forms import ModelForm,Textarea,TextInput
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User, Group
-
+from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    group = forms.ModelChoiceField(queryset=Group.objects.all(),widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Группа'}))
+    groups = forms.ModelChoiceField(queryset=Group.objects.all(),empty_label="Ничего не выбрано",widget=forms.Select(attrs={'class': 'form-control', 'placeholder': 'Группа'}))
     fullname = forms.CharField(max_length=150, required=True,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'ФИО'}))
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
@@ -14,4 +15,4 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields =UserCreationForm.Meta.fields +  ('group', 'fullname')
+        fields =UserCreationForm.Meta.fields +  ('fullname',)
