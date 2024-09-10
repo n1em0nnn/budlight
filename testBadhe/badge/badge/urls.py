c95b783e50cae123c62c885212f3a3f720f7ddb5
FnaRZ <fnarzgg@mail.ru>
2024-09-10 16:03:33 +0500

    add news

--- testBadhe/badge/badge/urls.py
+++ testBadhe/badge/badge/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -8,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('badges/', include('badges.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
 ]
 
 urlpatterns += [
